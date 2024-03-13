9b012449d7df7da0225795ddf1cf28ab68e01da0
Demian Rymar <ua.demyan@gmail.com>
2024-03-13 20:06:49 +0200

    Add project files.

---
+++ LAB_8.1_CHAR/Main.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int Count(const char* s) {
+    int k = 0;
+    size_t pos = 0;
+    while (s[pos] != '\0') {
+        if (s[pos] == '*' && s[pos + 1] == '*' && s[pos + 2] == '*')
+            k++;
+        pos++;
+    }
+    return k;
+}
+
+
+char* Change(char* s) {
+    size_t len = strlen(s);
+    char* t = new char[len * 2 + 1];
+    size_t pos1 = 0;
+    size_t pos2 = 0;
+
+    while (s[pos1] != '\0') {
+        if (s[pos1] == '*' && s[pos1 + 1] == '*' && s[pos1 + 2] == '*') {
+            t[pos2++] = '!';
+            t[pos2++] = '!';
+            pos1 += 3;
+        }
+        else {
+            t[pos2++] = s[pos1++];
+        }
+    }
+    t[pos2] = '\0';
+
+    for (size_t i = 0; i <= pos2; ++i) {
+        s[i] = t[i];
+    }
+
+    delete[] t;
+    return s;
+}
+
+
+int main() {
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+    cout << "String contained " << Count(str) << " groups of '***'" << endl;
+    Change(str);
+    cout << "Modified string (param) : " << str << endl;
+    return 0;
+}
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB_8.1_CHAR/Main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestCount) {
+			char str1[] = "***bbbbcccc***";
+			char str2[] = "***abcdabcdabcd";
+			char str3[] = "abcdefg";
+
+			int count1 = Count(str1);
+			int count2 = Count(str2);
+			int count3 = Count(str3);
+
+			Assert::AreEqual(2, count1);
+			Assert::AreEqual(1, count2);
+			Assert::AreEqual(0, count3);
+		}
+
+        TEST_METHOD(TestChange) {
+            char str1[] = "a***bc***def***";
+			char exp1[] = "a!!bc!!def!!";
+            Change(str1);
+            Assert::AreEqual(exp1, str1);
+
+			char str2[] = "******";
+			char exp2[] = "!!!!";
+			Change(str2);
+			Assert::AreEqual(exp2, str2);
+        }
+	};
+}
